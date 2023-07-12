e55e65cca2ab5b9ebc3e4f35a3fbdce14a930e52
VanGiau2002 <nguyenvangiau13012002@gmail.com>
2023-07-12 09:12:19 +0700

    new

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'giau.rx2x00u.mongodb.net',
+    DB_USER: 'giau',
+    DB_PASS: '13012002',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'vangiau13012002@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'wrywuljbewhoiheh',
+    JWT_SECRET: 'giau',
+    JWT_EXPIRES: '1000000', // in milliseconds
+  };
+  module.exports = MyConstants;
+  
